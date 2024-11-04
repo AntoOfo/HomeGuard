a43aefc6d15023bc2963b40e4aa790a20333f99d
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-11-04 00:01:29 +0000

    XML components initialised and TempTile Dialog function created

--- app/src/main/java/com/example/homeguard/MainActivity.kt
+++ app/src/main/java/com/example/homeguard/MainActivity.kt
@@ -1,14 +1,61 @@
 package com.example.homeguard
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainStatus: TextView
+    private lateinit var fireStatus: TextView
+    private lateinit var gasStatus: TextView
+    private lateinit var floodStatus: TextView
+    private lateinit var tempStatus: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fireTile = findViewById<CardView>(R.id.fireTile)
+        val gasTile = findViewById<CardView>(R.id.gasTile)
+        val floodTile = findViewById<CardView>(R.id.floodTile)
+        val tempTile = findViewById<CardView>(R.id.temperatureTile)
+
+        mainStatus = findViewById(R.id.statusText)
+        mainStatus.text = "Status: All Systems Normal"
+        fireStatus = findViewById(R.id.fireStatus)
+        fireStatus.text = "No"
+        gasStatus = findViewById(R.id.gasStatus)
+        gasStatus.text = "Safe"
+        floodStatus = findViewById(R.id.floodStatus)
+        floodStatus.text = "Low"
+        tempStatus = findViewById(R.id.temperatureStatus)
+        tempStatus.text = "Normal"
+
+        fireTile.setOnClickListener {
+
         }
+
+        gasTile.setOnClickListener {
+
+        }
+
+        floodTile.setOnClickListener {
+
+        }
+
+        tempTile.setOnClickListener {
+            showTempDetailsDialog()
+        }
+
+        }
+
+    private fun showTempDetailsDialog() {
+        // builds/shows detailed AlertDialog
+    }
     }
