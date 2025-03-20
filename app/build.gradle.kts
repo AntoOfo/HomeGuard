635a6d106794572d8371f91aaba84a8f0f81e4fd
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-03-20 14:39:26 +0000

    changed buttons to floating action buttons and also changed icons for cleaner look. brought in android material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-database")
+    implementation("com.google.android.material:material:1.11.0")
 }
